2f93d966683c448849cfaf4e85dc9a2fec238e6e
TwoDog-WANG <57378357+TwoDog-WANG@users.noreply.github.com>
2022-03-26 16:43:55 +0800

    添加注释

--- src/basicFunction/deepCopy.js
+++ src/basicFunction/deepCopy.js
@@ -1,10 +1,12 @@
 function deepCopy(ele) {
+//是数组，生成新的数组，对内部元素深拷贝，返回新的数组
     if( Array.isArray(ele) ) {
         let arr = ele.map((value) => {
             return deepCopy(value)
         })
         return arr
     }
+//对象同理
     else if( ele instanceof Object ) {
         let obj = {};
         for (const key in ele) {
@@ -12,6 +14,7 @@ function deepCopy(ele) {
         }
         return obj
     }
+//基本元素直接返回
     else {
         return ele
     }
